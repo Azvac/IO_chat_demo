3fc1dcc0a8791097702ec403d38e924181091342
Azvac <hugodeschamps2001@gmail.com>
2022-01-24 11:29:13 -0500

    Ajout de la fonctionalité d'utilisateur

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ io.sockets.on('connection', function(socket){
 
     // Déconnexion
     socket.on('disconnect', function(data){
+   //     if(!socket.username){} return;
+        users.splice(users.indexOf(socket.username), 1);
+        updateUsernames();
+
         connections.splice(connections.indexOf(socket), 1);
         console.log('Déconnecter %s sockets connected', connections.length);
     });
@@ -25,6 +29,19 @@ io.sockets.on('connection', function(socket){
     // Envoyer des message
     socket.on('send message', function(data){
         console.log(data);
-        io.sockets.emit('new message', {msg: data});
+        io.sockets.emit('new message', {msg: data, user: socket.username});
     });
+
+    // Nouvelle utilisateur
+    socket.on('new user', function(data, callback){
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUsernames();
+    });
+
+    function updateUsernames(){
+        io.sockets.emit('get users', users);
+
+    }
 });
